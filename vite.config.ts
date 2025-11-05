79a31bc758ab6f864349dce51c62912845a60ceb
kratsokcin <kratsokcin@gmail.com>
2025-11-05 10:19:47 +0100

    fixed a boo boo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 
-const repoName = 'nickostark.github.io'
+const repoName = 'homepage'
 
 export default defineConfig({
   base: `/${repoName}/`,
